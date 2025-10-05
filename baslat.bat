174689707c507ecee37d1953504e5daf4a4da0cb
ByLeJardin <77590664+ByLeJardi@users.noreply.github.com>
2025-10-05 11:14:02 +0300

    Create baslat.bat

---
+++ baslat.bat
@@ -0,0 +1,11 @@
+@echo off
+title ByLeJardin Minecraft Telegram Botu V0.1
+echo Bot baslatiliyor... Lutfen bekleyin.
+echo.
+
+python bot.py
+
+echo.
+echo Bot durduruldu veya bir hatayla karsilasti.
+echo Pencereyi kapatmak icin herhangi bir tusa basin.
+pause > nul
